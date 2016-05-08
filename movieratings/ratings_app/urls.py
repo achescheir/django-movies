5931d4dd7c6b34a393bf325ff72115bd0cbc1382
Alex Chescheir <alexchescheir@Nancys-MacBook-Pro.local>
2016-05-08 15:17:10 -0400

    added ability to add and update rating from movie page

--- movieratings/ratings_app/urls.py
+++ movieratings/ratings_app/urls.py
@@ -6,6 +6,8 @@ app_name = "ratings_app"
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^movie(?P<id>[0-9]+)/add_rating/', views.add_rating, name="rate_movie"),
+    url(r'^movie(?P<id>[0-9]+)/update_rating/', views.update ,name = "submit_rating" ),
     url(r'^movie(?P<id>[0-9]+)/', views.movie_detail_view, name="movie_detail_url"),
     url(r'^user(?P<id>[0-9]+)/', views.rater_detail, name="rater_detail"),
     url(r'^top(?P<num>[0-9]+)/', views.top_movies, name="top_movies"),
--- movieratings/ratings_app/views.py
+++ movieratings/ratings_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import HttpResponse, render, get_object_or_404
+from django.shortcuts import HttpResponse, render, get_object_or_404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from .models import Movie, Rating, Rater
+from datetime import datetime
 
 
 # Create your views here.
@@ -20,3 +22,21 @@ def top_movies(request, num):
 
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+def add_rating(request, id):
+    movie = get_object_or_404(Movie, pk=id)
+    return render(request, 'raters/add_rating.html', {'movie': movie,"ratingchoices":[1,2,3,4,5]})
+
+def update(request, id):
+    movie = Movie.objects.get(id=id)
+    rater = Rater.objects.get(id=request.POST['rater'])
+    rating_value = request.POST['rating']
+    if len(Rating.objects.filter(movie=movie).filter(rater=rater)) == 0:
+        new_rating = Rating(movie=movie, rater=rater,rating_value=rating_value,time=datetime.now())
+        new_rating.save()
+    else:
+        old_rating = Rating.objects.filter(movie=movie).get(rater=rater)
+        old_rating.rating_value = rating_value
+        old_rating.time = datetime.now()
+        old_rating.save()
+    return HttpResponseRedirect(reverse('ratings_app:rater_detail', args=(rater.id,)))
